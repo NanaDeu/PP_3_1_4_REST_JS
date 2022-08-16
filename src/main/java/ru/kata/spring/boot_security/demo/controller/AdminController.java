d0b1e1f165fa9601a249b64829b4caf1c72ba8be
NanaDeu <nd.nat.nd@gmail.com>
2022-08-16 14:21:16 +0700

    commit 4 3_1_3 Работает

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -2,14 +2,12 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.security.Principal;
-import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -32,11 +30,14 @@ public class AdminController {
         model.addAttribute("newUser", new User());
         model.addAttribute("listRoles", userService.listRoles());
 
-        //edit
-/*        model.addAttribute("editUser", userService.getUserById(id));
-        model.addAttribute("listRoles", userService.listRoles());*/
-
         return "admin";
+    }
+
+    @PatchMapping("/edit/{id}")
+    public String updateUser(@ModelAttribute("userTable") User user, @PathVariable("id") long id) {
+        userService.updateUser(id, user);
+
+        return "redirect:/admin";
     }
 
     @PostMapping()
@@ -50,63 +51,4 @@ public class AdminController {
         userService.removeUserById(id);
         return "redirect:/admin";
     }
-
-/*    @GetMapping("/edit")
-    @ResponseBody
-    public String updateUser(long id, Model model) {
-        model.addAttribute("editUser", userService.getUserById(id));
-        model.addAttribute("listRoles", userService.listRoles());
-        return "redirect:/admin";
-    }
-
-    @PatchMapping("/edit")
-    public String updateUser(@ModelAttribute("editUser") User user, long id) {
-        userService.updateUser(id, user);
-        return "redirect:/admin";
-    }*/
-
-/*    @Controller
-    @RequestMapping("/admin/users")
-    public class AdminController*/
-
-   /* @GetMapping()
-    public String printUsers(ModelMap model) {
-        List<User> list = userService.getAllUsers();
-        model.addAttribute("userList", list);
-        return "/users";
-    }
-
-
-    @GetMapping("/new")
-    public String addUserForm(ModelMap model) {
-        model.addAttribute("user", new User());
-        model.addAttribute("listRoles", userService.listRoles());
-        return "new";
-    }
-
-    @PostMapping()
-    public String addUser(@ModelAttribute("user") User user) {
-        userService.save(user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/edit/{id}")
-    public String updateUser(@PathVariable("id") int id, Model model) {
-        model.addAttribute("user", userService.getUserById(id));
-        model.addAttribute("listRoles", userService.listRoles());
-        return "/edit";
-    }
-
-    @PatchMapping("/edit/{id}")
-    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") int id) {
-        userService.updateUser(id, user);
-        return "redirect:/admin/users";
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id) {
-        userService.removeUserById(id);
-        return "redirect:/admin/users";
-    }*/
-
 }
--- src/main/java/ru/kata/spring/boot_security/demo/controller/HelloController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/HelloController.java
@@ -4,14 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.security.Principal;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class HelloController {
@@ -24,34 +20,13 @@ public class HelloController {
 
     @GetMapping(value = "/")
     public String printWelcome(ModelMap model) {
-        List<String> messages = new ArrayList<>();
-        messages.add("Hello!");
-        messages.add("Here you can take a look at all Users(if you are an admin)");
-        model.addAttribute("messages", messages);
         return "login";
     }
-/*    @GetMapping("/admin")
-    public String pageForAdmin(Principal principal, ModelMap model) {
-        User user = userService.findUserByUsername(principal.getName());
-        model.addAttribute("user", user);
 
-        List<User> list = userService.getAllUsers();
-        model.addAttribute("userList", list);
-
-        //new
-        model.addAttribute("newUser", new User());
-        model.addAttribute("listRoles", userService.listRoles());
-        return "admin";
-    }
-    @PostMapping("/admin")
-    public String addUser(@ModelAttribute("newUser") User newUser) {
-        userService.save(newUser);
-        return "admin";
-    }*/
     @GetMapping("/user")
     public String pageForUser(Principal principal, ModelMap model) {
-        /*User user = userService.findUserByUsername(principal.getName());*/
-        model.addAttribute("userDescription", userService.findUserByUsername(principal.getName()).toString());
+        User user = userService.findUserByUsername(principal.getName());
+        model.addAttribute("user", user);
         return "user";
     }
 }
